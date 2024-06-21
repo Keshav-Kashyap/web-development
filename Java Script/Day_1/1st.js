06789d54c6c3a1524691ef9af59c9e91638089ac
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-06-21 14:16:44 +0530

    Add files via upload

---
+++ Java Script/Day_1/1st.js
@@ -0,0 +1,16 @@
+console.log("hello world"); 
+var a=5;// no data type
+console.log(a);// no formate strings
+  a="keshav"// kisi main bhi change kardo 
+console.log(a);
+a=7.56// ab float main change kar diya
+console.log(a);
+//Opertaors, +,-,*,/
+var a=5;// var se dobra decalre kar skate hai
+// const a=6;// gives ERROR this is not allowed matlb alg data type se decalre nhi kar skte
+// console.log(a); 
+b=4;
+console.log(a+b);// \n Ki jaruat bhi nhi 
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);// pura answer dega decimal main bhi 
