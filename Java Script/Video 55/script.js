031e137def46fd01dfa153776a5d3078ce297aa9
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-09-25 08:01:25 +0530

    Add files via upload

---
+++ Java Script/Video 55/script.js
@@ -0,0 +1,11 @@
+console.log("this is tutorial  55");
+let a=5;
+var b=4;
+var c= "keshav";
+console.log(a+b);
+console.log(c);
+console.log(typeof a , typeof b, typeof c);
+const a1=4;
+//a1=a1+1; // invailed
+
+
