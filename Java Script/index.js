7a5f9a43f89bd07de0e512a412540cdb9717d61e
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-09-25 18:02:57 +0530

    Add files via upload

---
+++ Java Script/index.js
@@ -0,0 +1,22 @@
+// console.log("hi Video 57");
+// let i;
+// for(i=0;i<=10;i++){
+//     console.log(i);
+// }
+// let obj= {
+//     name:"keshav",
+//     role:"programmer",
+//     comany:"keshav and keshav"
+// }
+// for(const key in obj) {
+//     console.log(key);
+
+// }
+// for (const  c of "mayank") {
+//     console.log(c)
+// }
+let i=1;
+while(i<6){
+    console.log(i);
+    i++;
+}
