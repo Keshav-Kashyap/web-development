bdd7b7bdeda0a5a9c4ab7a6ca663dacf34f3a17f
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-09-26 07:40:43 +0530

    Add files via upload

---
+++ Java Script/Video 58/index.js
@@ -0,0 +1,23 @@
+console.log("functions in javascript...");
+
+function nice(name) {
+    console.log("Hey " + name + " your nice");
+    console.log("hye " + name + " you are good");
+
+
+}
+function sum(a, b) {
+
+    // console.log(a+b);
+    return a + b;
+}
+// sum(4,5);
+result =  sum(4, 5);
+result2 = sum(3,4);
+result3 = sum(5,6);
+
+console.log(result);
+console.log(result2);
+console.log(result3);
+nice("rohan");
+
