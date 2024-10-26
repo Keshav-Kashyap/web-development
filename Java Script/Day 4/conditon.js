6cb78d0020a3162d005f3d6fd1270c0b1035c41f
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-10-26 23:08:31 +0530

    Add files via upload

---
+++ Java Script/Day 4/conditon.js
@@ -0,0 +1,15 @@
+let age=18;
+if(age>18){
+    console.log("You can vote becouse your age: " + age +" year");
+}
+else if(age==18){
+
+console.log("Oh!!, Nice This is your first time to vote , Your age " + age);
+}
+else if(age==1){
+    console.log("Hey, You are a baby, Your age " + age);
+}
+else (age==1)
+{
+    console.log("Hey, You can not vote, Your age " + age);
+}
