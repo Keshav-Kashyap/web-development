2e88b377eb638fad168d0a95f755089d234d163a
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-11-13 23:12:47 +0530

    Add files via upload

---
+++ Java Script/Day 14/script.js
@@ -0,0 +1,10 @@
+// let para=document.querySelector("p");
+// let heading=document.querySelector('h1')
+// // heading.innerHTML = "<u> keshav </u>";
+// // console.dir(heading);
+// heading.innerHTML =` <u> ${heading.innerText}</u>`
+
+let img = document.querySelector("img");
+img.getAttribute("id");
+img.setAttribute("id","spder-img");
+img.setAttribute("class","images");
