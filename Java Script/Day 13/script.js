8f8e2e0649683109e874975c62abbf5f544e017f
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-11-13 23:10:51 +0530

    Add files via upload

---
+++ Java Script/Day 13/script.js
@@ -0,0 +1,2 @@
+let change = document.getElementsByClassName("txt");
+change[0].innerHTML="kashap"
---
+++ Java Script/Day 13/script2.js
@@ -0,0 +1,11 @@
+// let change = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < change.length; i++) {
+//     change[i].src = "https://img.freepik.com/free-photo/colorful-design-with-spiral-design_188544-9588.jpg";
+
+// }
+
+// document.getElementsByTagName("p")[1].innerText="keshav";
+console.dir(document.querySelectorAll("p"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+console.dir(document.querySelectorAll("div a"));
