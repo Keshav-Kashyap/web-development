c5d39e4b2d839b3826aa1f040135ebfc8c317bc3
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2024-12-04 23:51:24 +0530

    Add files via upload

---
+++ destructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Example {
+    public:
+    void show(){
+        cout<<Showing Example....<<endl;
+    }
+    Example{
+        delete Example;
+        
+    }
+};
+
+
+
+
+int main  (){
+
+
+
+
+
+    return 0;
+}
