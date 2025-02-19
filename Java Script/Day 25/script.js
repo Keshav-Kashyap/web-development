ff3441937c8ff0ea8bbaaf7a78b6315834d2648d
keshav kashyap <168452278+Keshav-Kashyap@users.noreply.github.com>
2025-02-20 00:23:32 +0530

    Add files via upload

--- Java Script/Day 25/script.js
+++ Java Script/Day 25/script.js
@@ -28,9 +28,51 @@
 
 
 
-let jsonRes=
+// let jsonRes=
 
-'{"fact":"The tiniest cat on record is Mr. Pebbles, a 2-year-old cat that weighed 3 lbs (1.3 k) and was 6.1 inches (15.5 cm) high.","length":120}';
+// '{"fact":"The tiniest cat on record is Mr. Pebbles, a 2-year-old cat that weighed 3 lbs (1.3 k) and was 6.1 inches (15.5 cm) high.","length":120}';
 
-let validRes= JSON.parse(jsonRes);
-console.log(validRes.fact);
+// let validRes= JSON.parse(jsonRes);
+// console.log(validRes.fact);
+
+
+let url = "https://catfact.ninja/fact";
+// fetch(url)
+//     .then((res) => {
+      
+//         return res.json()
+//     })
+//     .then((data)=>{
+//         console.log(data.fact);
+//         return fetch(url);
+//     })
+//     .then((res)=>{
+//         return res.json()
+//     }).then((data2)=>{
+//         console.log(data2.fact);
+//     })
+
+//     .catch((err) => {
+//         console.log(err);
+//     })
+
+async function getFacts() {
+    
+   try{
+    let res= await fetch(url);
+    let data=await res.json();
+    console.log(data.fact);
+
+    let res2= await fetch(url);
+    let data2=await res2.json();
+    console.log(data2.fact);
+
+
+
+
+
+   }catch(e){
+    console.log("error- ",e);
+   }
+
+}
